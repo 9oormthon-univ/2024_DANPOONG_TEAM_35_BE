78ab045c055570a68dded80a54d1d53e2fefaf7b
jiwonLee42 <ljw56b@gmail.com>
2024-11-23 17:16:26 +0900

    [CHORE] 업계소식, 경험시트 키워드 필터 API 수정

--- src/main/java/app/jasople/Keywords/controller/KeywordController.java
+++ src/main/java/app/jasople/Keywords/controller/KeywordController.java
@@ -1,11 +1,14 @@
 package app.jasople.Keywords.controller;
 
 import app.jasople.Config.ApiResponse;
+import app.jasople.Keywords.dto.KeywordFilterResponseDto;
 import app.jasople.Keywords.dto.KeywordResponseDto;
 import app.jasople.Keywords.entity.keywordType;
 import app.jasople.Keywords.service.KeywordService;
+import app.jasople.security.CustomUserDetail;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +38,21 @@ public class KeywordController {
         return ApiResponse.onSuccess(keywords);
     }
 
-    // 키워드 집계
+    // 키워드 집계 - 경험시트
+    @GetMapping("/filter-experience")
+    @Operation(summary = "경험시트 키워드 필터 API", description = "경험시트 키워드를 개수를 조회합니다.")
+    public ApiResponse<List<KeywordFilterResponseDto>> filterExperienceKeywords(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        List<KeywordFilterResponseDto> keywords = keywordService.filterKeywordsInEx(userDetail.getUser());
+        return ApiResponse.onSuccess(keywords);
+    }
+
+    // 키워드 집계 - 업계소식
+    @GetMapping("/filter-experience")
+    @Operation(summary = "업계소식 키워드 필터 API", description = "업계소식 키워드를 개수를 조회합니다.")
+    public ApiResponse<List<KeywordFilterResponseDto>> filterInfoKeywords(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        List<KeywordFilterResponseDto> keywords = keywordService.filterKeywordsInInfo(userDetail.getUser());
+        return ApiResponse.onSuccess(keywords);
+    }
 
 
 
